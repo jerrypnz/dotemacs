b1e3060405240eba1f4a4bdbf34c3ca954721ac4
Jerry Peng <pr2jerry@gmail.com>
2012-10-18 11:13:23 +0800

    Add README and merge font setting

--- conf.d/fonts.el
+++
@@ -1,9 +0,0 @@
-;; Setting fonts for GUI emacs
-(if (display-graphic-p)
-    (progn
-      (set-face-attribute
-       'default nil :font "Ubuntu Mono 12")
-      (dolist (charset '(kana han symbol cjk-misc bopomofo))
-        (set-fontset-font (frame-parameter nil 'font)
-                          charset
-                          (font-spec :family "文泉驿等宽微米黑" :size 16)))))
--- conf.d/settings.el
+++ conf.d/settings.el
@@ -1,6 +1,16 @@
 ;; Color theme
 (load-theme 'solarized-light t)
 
+;; Setting fonts for GUI emacs
+(if (display-graphic-p)
+    (progn
+      (set-face-attribute
+       'default nil :font "Ubuntu Mono 12")
+      (dolist (charset '(kana han symbol cjk-misc bopomofo))
+        (set-fontset-font (frame-parameter nil 'font)
+                          charset
+                          (font-spec :family "文泉驿等宽微米黑" :size 16)))))
+
 ;; Change auto-save dir
 (defvar my-backup-dir (concat temporary-file-directory "emacs-backup"))
 
