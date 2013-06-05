e1f48c0ea9ec55fe4734027cdae9bcd644af3d1e
Jerry Peng <Jerry Peng>
2013-06-05 17:07:35 +0800

    Disable blog.el since org2bog is outdated

--- conf.d/blog.el
+++
@@ -1,7 +0,0 @@
- (setq org2blog/wp-blog-alist
-       '(("wordpress"
-          :url "http://jerrypeng.me/xmlrpc.php"
-          :username "Jerry"
-          :default-title "Hello World"
-          :default-categories ("org2blog" "emacs")
-          :tags-as-categories nil)))
