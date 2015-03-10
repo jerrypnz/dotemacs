83e5f788d169db2a62011ec8d82298dea90bdd70
Jerry Peng <jerry@movio.co>
2015-03-10 15:21:57 +1300

    Add fancy unicode arrow support to scala mode

--- conf.d/scala.el
+++ conf.d/scala.el
@@ -1,5 +1,29 @@
-; Hooks for helm-gtags
+;; Hooks for helm-gtags
 (add-hook 'scala-mode-hook 'helm-gtags-mode)
 (add-hook 'java-mode-hook 'helm-gtags-mode)
+
+
+;; Fancy unicode arrows
+(defun right-arrow ()
+  (interactive)
+  (cond ((looking-back "=") 
+         (backward-delete-char 1) (insert "⇒"))
+	((looking-back "-")
+	 (backward-delete-char 1) (insert "→"))
+	(t (insert ">"))))
+ 
+(defun left-arrow ()
+  (interactive)
+  (if (looking-back "<") 
+      (progn (backward-delete-char 1)
+	     (insert "←"))
+    (insert "-")))
+ 
+ 
+(add-hook 'scala-mode-hook '(lambda () (interactive) 
+			      (local-set-key (kbd "-") 'left-arrow)
+			      (local-set-key (kbd ">") 'right-arrow)))
+
+ 
 
 
