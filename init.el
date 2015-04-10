97a9ac01c32a9634986bf5a6542a1212ab0ac140
Jerry Peng <jerry@movio.co>
2015-04-10 14:51:04 +1200

    Add haskell related config

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
