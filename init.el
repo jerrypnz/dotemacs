2deed858459933d2ecd106dddc56926d88aac317
Jerry Peng <jerry@movio.co>
2016-08-02 11:54:01 +1200

    Change the default theme to material

--- init.el
+++ init.el
@@ -138,9 +138,9 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(twilight-anti-bright
+   dotspacemacs-themes '(material
+                         twilight-anti-bright
                          monokai
-                         material
                          spacegray
                          omtose-phellack
                          spacemacs-dark)
